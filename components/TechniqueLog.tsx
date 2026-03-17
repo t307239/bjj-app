9b7f23d0dfdedcf48a505bbbb619ab12c9a8618a
t307239 <61605432+t307239@users.noreply.github.com>
2026-03-17 17:44:23 +0900

    P5: add YouTube search button on each technique card in TechniqueLog

--- components/TechniqueLog.tsx
+++ components/TechniqueLog.tsx
@@ -603,6 +603,16 @@ export default function TechniqueLog({ userId }: Props) {
                     )}
                   </div>
                   <div className="flex gap-1 ml-3 flex-shrink-0">
+                    {/* YouTube検索ボタン */}
+                    <button
+                      onClick={() => window.open(`https://www.youtube.com/results?search_query=${encodeURIComponent(technique.name + " BJJ tutorial")}`, "_blank", "noopener,noreferrer")}
+                      className="text-gray-600 hover:text-red-400 transition-colors p-1"
+                      title="YouTubeで検索"
+                    >
+                      <svg className="w-4 h-4" viewBox="0 0 24 24" fill="currentColor">
+                        <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+                      </svg>
+                    </button>
                     <button
                       onClick={() => startEdit(technique)}
                       className="text-gray-600 hover:text-blue-400 transition-colors p-1"
