7e829c2e9fd807fe56b578632924e31805bd3fd0
Toshiki <307239t777@gmail.com>
2026-04-15 18:00:51 +0900

    auto-commit: 2026-04-15 18:00 [daemon]

--- components/skillmap/AddNodePopup.tsx
+++ components/skillmap/AddNodePopup.tsx
@@ -17,6 +17,8 @@ export default function AddNodePopup({ screenX, screenY, onAdd, onCancel, t }: P
 
   return (
     <div
+      role="dialog"
+      aria-label={t("skillmap.addTechnique")}
       style={{ position: "fixed", left: screenX, top: screenY, zIndex: 30 }}
       className="bg-zinc-800 border border-white/20 rounded-xl shadow-2xl p-3 w-48"
       onMouseDown={(e) => e.stopPropagation()}
